7b3ca41fbd901dc640e3a2ce527e781b252292d3
Philipp <p.tuerk90@gmail.com>
2017-05-11 20:37:04 +0200

    * Überprüfung der Dateikopie durch Prüfsumme (MD5)

--- src/main/java/de/urkallinger/copymanager/callables/FileCopier.java
+++ src/main/java/de/urkallinger/copymanager/callables/FileCopier.java
@@ -1,7 +1,12 @@
 package de.urkallinger.copymanager.callables;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
@@ -62,17 +67,42 @@ public class FileCopier implements Runnable {
 
 	private Runnable getCpyFileRunnable(File from, File to) {
 		return () -> {
-			String action = String.format(Str.get("FileCopier.copy_action"), 
-					from.getAbsolutePath(),
+			String action = String.format(Str.get("FileCopier.copy_action"), from.getAbsolutePath(),
 					to.getAbsolutePath());
 			int idx = MainApp.getLogger().action(action, true);
 			try {
 				FileUtils.copyFile(from, to);
-				MainApp.getLogger().setDone(idx);
-			} catch (IOException e) {
+				
+				if(md5(from).equals(md5(to))) {
+					MainApp.getLogger().setDone(idx);
+				} else {
+					MainApp.getLogger().setFailed(idx);
+					MainApp.getLogger().error(Str.get("FileCopier.checksum_error"));
+				}
+			} catch (IOException | NoSuchAlgorithmException e) {
 				MainApp.getLogger().setFailed(idx);
 				MainApp.getLogger().error(e.getMessage());
 			}
 		};
+	}
+
+	private static String md5(File f) throws NoSuchAlgorithmException, IOException {
+		InputStream is = new FileInputStream(f);
+		try {
+			MessageDigest digest = MessageDigest.getInstance("MD5");
+			byte[] buffer = new byte[8192];
+			int read = 0;
+
+			while ((read = is.read(buffer)) > 0) {
+				digest.update(buffer, 0, read);
+			}
+			
+			byte[] md5sum = digest.digest();
+			BigInteger bigInt = new BigInteger(1, md5sum);
+			
+			return bigInt.toString(16);
+		} finally {
+			try { is.close(); } catch (Exception e) {}
+		}
 	}
 }
